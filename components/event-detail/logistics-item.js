0a7b86df40977f396a61f38056921e2c2cff6021
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-08-03 12:36:27 +0400

    Added Event Detail

---
+++ components/event-detail/logistics-item.js
@@ -0,0 +1,15 @@
+import classes from './styles/logistics-item.module.css';
+
+const LogisticsItem = ({children, icon: Icon }) => {
+
+  return (
+    <li className={classes.item}>
+      <span className={classes.icon}>
+        <Icon />
+      </span>
+      <span className={classes.content}>{children}</span>
+    </li>
+  );
+}
+
+export default LogisticsItem;
