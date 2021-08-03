495029a999b0753557627c4465c0c75a98c5e448
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-08-03 12:28:33 +0400

    Add icon and button on event-item

--- components/event/event-item.jsx
+++ components/event/event-item.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import Link from "next/link"
+import Button from "../button/button";
+import DateIcon from "../icons/date-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import AddressIcon from "../icons/address-icon";
+
+
 import classes from "./styles/event-item.module.css"
 
 const EventItem = ({ item }) => {
@@ -18,19 +23,26 @@ const EventItem = ({ item }) => {
     
     return(
         <li className={classes.item}>
-            <img src={`/${image}`} alt="event image" />
+            <div className={classes.img__container}>
+              <img src={`/${image}`} alt="event image" />  
+            </div>
             <div className={classes.content}>
                 <div className={classes.summary}>
-                    <h1>{title}</h1>
+                    <h2>{title}</h2>
                     <div className={classes.date}>
+                        <DateIcon />
                         <time>{readableDate}</time>
                     </div>
                     <div className={classes.address}>
+                        <AddressIcon />
                         <address>{fomrattedAddress}</address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Link href={exploreLink}>Explore Event</Link>
+                    <Button link={exploreLink}>
+                        <span>Explore Event</span>
+                        <span className={classes.icon}><ArrowRightIcon /></span>
+                    </Button>
                 </div>
             </div>
         </li>
