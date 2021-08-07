cf9563e1606906109c9dfd034b78833e9a21b48e
Mohammad Oumeir Rengony <oumeir.rengony@hangarww.com>
2021-08-07 12:18:26 +0400

    fix for _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Html, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
     render() {
@@ -6,6 +6,7 @@ class MyDocument extends Document {
             <Html  lang="en">
                 <Head />
                 <body>
+                    <Main />
                     <NextScript />
                 </body>
             </Html>
